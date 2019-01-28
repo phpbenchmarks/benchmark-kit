deed88ea9ea9f4f95cf33847811aafbfa13c35f2
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-01-28 14:28:02 +0100

    Fix error message

--- validation/configurationValidation.sh
+++ validation/configurationValidation.sh
@@ -100,9 +100,9 @@ function assertInitBenchmark {
     assertConfigurationFileExist "initBenchmark.sh"
 
     source $CONFIGURATION_PATH/initBenchmark.sh
-    [ "$?" != "0" ] && exitScript "File init_benchmark.sh could not be included."
+    [ "$?" != "0" ] && exitScript "File .phpbenchmarks/initBenchmark.sh could not be included."
 
     type initBenchmark &>/dev/null
-    [ "$?" != "0" ] && exitScript "Function init_benchmark.sh::initBenchmark() does not exist. See README.md for more informations."
+    [ "$?" != "0" ] && exitScript "Function .phpbenchmarks/initBenchmark.sh::initBenchmark() does not exist. See README.md for more informations."
     echoValidatedTest "[.phpbenchmarks/initBenchmark.sh] Function initBenchmark() exist."
 }
