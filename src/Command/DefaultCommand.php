beb71a11947cae88454127c7f86a9e822b41837b
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-11-27 16:20:47 +0100

    Add --source and --nginx-port parameters to phpbenchkit, add source code path into DefaultCommand

--- src/Command/DefaultCommand.php
+++ src/Command/DefaultCommand.php
@@ -30,7 +30,8 @@ final class DefaultCommand extends ListCommand
             [
                 '',
                 'Welcome to http://www.phpbenchmarks.com benchmark kit ' . Version::getVersion() . '.',
-                ''
+                '',
+                'Host source code path: ' . getenv('HOST_SOURCE_CODE_PATH') . '.'
             ],
             $output,
             $isConfigurationValid
