5341c909366c6f36d77bbbff2330597abc7cedc4
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-04-24 00:44:21 +0200

    Dockerise everything

--- src/Benchmark/BenchmarkUrlService.php
+++ src/Benchmark/BenchmarkUrlService.php
@@ -46,7 +46,12 @@ class BenchmarkUrlService
 
     public static function getPreloadGeneratorUrl(): string
     {
-        return 'http://' . static::PRELOAD_GENERATOR_HOST . ':' . static::getNginxPort() . Benchmark::getBenchmarkRelativeUrl();
+        return
+            'http://'
+            . static::PRELOAD_GENERATOR_HOST
+            . ':'
+            . static::getNginxPort()
+            . Benchmark::getBenchmarkRelativeUrl();
     }
 
     public static function getNginxPort(): int
